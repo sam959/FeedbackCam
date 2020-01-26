96a91696c4f93e26e6547a9dae48eda9b87fbfc1
sam959 <liekemonster@gmail.com>
2020-01-26 18:05:11 +0100

    Working on zoom

--- app/src/main/assets/lumakey.vert.glsl
+++ app/src/main/assets/lumakey.vert.glsl
@@ -4,15 +4,19 @@ attribute vec4 position;
 //camera transform and texture
 uniform mat4 camTextureTransform;
 uniform mat4 uMVPMatrix;
+uniform float zoom;
 
 attribute vec4 camTexCoordinate;
 
 //tex coords
 varying vec2 v_CamTexCoordinate;
+varying vec2 v_zoomedCoords;
 
 void main()
 {
+    float scaledZoom = zoom * 0.1 + 0.95;
     //camera texcoord needs to be manipulated by the transform given back from the system
     v_CamTexCoordinate = (camTextureTransform * camTexCoordinate).xy;
+    v_zoomedCoords = (position.xy * scaledZoom) * 0.5 + 0.5;
     gl_Position =  position;
 }
--- app/src/main/assets/mirror.frag.glsl
+++ app/src/main/assets/mirror.frag.glsl
@@ -1,24 +1,27 @@
 #extension GL_OES_EGL_image_external : require
 //SOFT LUMA KEY
 //"CREDIT": "by IMIMOT (ported from http://www.memo.tv/)"
-
 precision highp float;
 uniform samplerExternalOES camTexture;
 uniform sampler2D bufferTexture;
 
 varying vec2 v_CamTexCoordinate;
+varying vec2 v_zoomedCoords;
 varying vec2 v_TexCoordinate;
+
+uniform float mirrorActive;
 
 uniform float threshold;
 uniform float opacity;
 uniform float hue;
+uniform float contrast;
+uniform float saturation;
+uniform float brightness;
 uniform float dispX;
 uniform float dispY;
 
-
 vec3 hueShift(vec3 color, float hueAdjust){
     //implemented by mairod
-
     vec3  kRGBToYPrime = vec3 (0.299, 0.587, 0.114);
     vec3  kRGBToI      = vec3(0.596, -0.275, -0.321);
     vec3  kRGBToQ      = vec3(0.212, -0.523, 0.311);
@@ -42,30 +45,46 @@ vec3 hueShift(vec3 color, float hueAdjust){
 
     vec3    yIQ   = vec3(YPrime, I, Q);
     return vec3(dot(yIQ, kYIQToR), dot(yIQ, kYIQToG), dot(yIQ, kYIQToB));
+}
 
+vec3 conSatBri(vec3 color, float brt, float sat, float con)
+{
+    // Increase or decrease theese values to adjust r, g and b color channels seperately
+    const float AvgLumR = 0.5;
+    const float AvgLumG = 0.5;
+    const float AvgLumB = 0.5;
 
+    const vec3 LumCoeff = vec3(0.2125, 0.7154, 0.0721);
+
+    vec3 AvgLumin  = vec3(AvgLumR, AvgLumG, AvgLumB);
+    vec3 brtColor  = color * brt;
+    vec3 intensity = vec3(dot(brtColor, LumCoeff));
+    vec3 satColor  = mix(intensity, brtColor, sat);
+    vec3 conColor  = mix(AvgLumin, satColor, con);
+
+    return conColor;
 }
 
 void main() {
-
     // Mirror
     float isAfterHalfWidth = step(0.5, v_CamTexCoordinate.x);
     float isAfterHalfHeight = step(v_CamTexCoordinate.y, 0.5);
 
-    float subX = ((v_CamTexCoordinate.x - 0.5) * 2.) * isAfterHalfWidth;
-    float subY = (1. - v_CamTexCoordinate.y) * isAfterHalfHeight;
+    float subX = ((v_zoomedCoords.x - 0.5) * 2.) * isAfterHalfWidth;
+    float subY = ((v_zoomedCoords.y - 0.5)*2.) * isAfterHalfHeight;
 
-    vec2 mirroredPixelCoords = vec2(v_CamTexCoordinate.x - subX, v_CamTexCoordinate.y);
+    vec2 mirroredPixelCoords = vec2(v_zoomedCoords.x, v_zoomedCoords.y - (subY * mirrorActive));
 
     // Displacement
-    float scaledDispX = (dispX * 0.1) - 0.05;
-    float scaledDispY = (dispY * 0.1)- 0.05;
+    float scaledDispX = (dispX * 0.06) - 0.03;
+    float scaledDispY = (dispY * 0.06)- 0.03;
 
     // Feedback
-    // vec2 feedbackCoord = vec2(-v_CamTexCoordinate.y, -v_CamTexCoordinate.x);
-    vec2 feedbackCoord = vec2(-mirroredPixelCoords.y, -mirroredPixelCoords.x);
+    //vec2 feedbackCoord = vec2(-v_CamTexCoordinate.y, -v_CamTexCoordinate.x);
+    vec2 feedbackCoord = mirroredPixelCoords.xy;
+    //vec2 zoomCoord = (feedbackCoord - 0.5) * (dispX*4.) + (0.5);
 
-    vec4 pix = texture2D(camTexture, mirroredPixelCoords);
+    vec4 pix = texture2D(camTexture, v_CamTexCoordinate.xy);
     vec4 feedback = texture2D(bufferTexture, feedbackCoord.xy - vec2(scaledDispX, scaledDispY));
 
     float newopacity = opacity * 0.15;
@@ -75,13 +94,13 @@ void main() {
 
     //float threshBW = (threshold * 2.0) - 1.0;
     float bw_sel = step(threshold, 0.0);
-    fValue = smoothstep(max(l1,0.0), min(l2, 1.0), abs(bw_sel - fValue));
+    fValue = smoothstep(max(l1, 0.0), min(l2, 1.0), abs(bw_sel - fValue));
     pix.a = fValue;
     float isKeyOpen = step(pix.a, 0.1);
     // final mix needed to make alpha working
-    vec4 color = mix(vec4(pix.rgb, 0.0),pix,pix.a);
-    feedback = vec4(hueShift(feedback.xyz, hue*6.0),1.0);
-    color = vec4(mix(color.rgb, feedback.rgb, 1.0 - pix.a - (isKeyOpen * newopacity)),  1.0);
-
+    vec4 color = mix(vec4(pix.rgb, 0.0), pix, pix.a);
+    feedback.xyz = hueShift(feedback.xyz, hue*3.0);
+    feedback.xyz = conSatBri(feedback.xyz, contrast+0.5, saturation + 0.5, brightness + 0.5);
+    color = vec4(mix(color.rgb, feedback.rgb, 1.0 - pix.a - (isKeyOpen * newopacity)), 1.0);
     gl_FragColor = color;
 }
